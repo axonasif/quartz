3f9608abe0f6933a36cbc104710fade12ce3fe13
Aminur Rahman <axonasif@gmail.com>
2025-10-04 21:37:35 +0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "axonasif.com",
+    pageTitle: "Asif",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
